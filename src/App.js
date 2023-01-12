ed487f12711a2fc36fbacee481b17c92aef9f642
HardikShah563 <hardikts@gmail.com>
2023-01-13 00:02:35 +0530

    made api call instead of getting it from js file full of array

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import './App.css';
-
 import Header from './Header';
 import Meme from './Meme';
 
 export default function App() {
   return (
-    <>
+    <div>
       <Header />
       <Meme />
-    </>
+    </div>
   )
 }
--- src/Meme.js
+++ src/Meme.js
@@ -1,55 +1,105 @@
 import React from 'react';
-import memesData from './memesdata'
+// import memesData from './memesdata'
 import './Meme.css';
+import boxes from './boxes.js'
+import Box from './Box.js'
 
 export default function Meme() {
     const [meme, setMeme] = React.useState({
-        topText:"",
-        randomImage:"https://i.imgflip.com/30b1gx.jpg",
-        bottomText:"",
+        topText: "",
+        randomImage: "https://i.imgflip.com/30b1gx.jpg",
+        bottomText: "",
     });
 
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData);
-    
+    const [allMemes, setAllMemes] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch("http://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, []);
+
+    // console.log(allMemes)
+
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes;
-        const randomNumber = Math.floor(Math.random() * memesArray.length);
-        console.log(memesArray[randomNumber].url);
-        const url = memesArray[randomNumber].url;
+        const randomNumber = Math.floor(Math.random() * allMemes.length);
+        const url = allMemes[randomNumber].url;
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url,
         }));
     }
 
+    const [formData, setFormData] = React.useState({
+        topText: "",
+        bottomText: "",
+    });
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [name]: value,
+        }))
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("form submit");
+    }
+
     return (
         <main>
-            <div className="form">
-                <input 
-                    className="form--input" 
-                    placeholder="Top Text" 
-                    type="text" 
-                />
-                
-                <input 
-                    className="form--input" 
-                    placeholder="Bottom Text" 
-                    type="text" 
-                />
-                
-                <button 
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="flex">
+                    <input
+                        className="form--input"
+                        placeholder="Top Text"
+                        type="text"
+                        name="topText"
+                        value={formData.topText}
+                        onChange={handleChange}
+                    />
+
+                    <input
+                        className="form--input"
+                        placeholder="Bottom Text"
+                        type="text"
+                        name="bottomText"
+                        value={formData.bottomText}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <button
                     className="fomt--button"
                     onClick={getMemeImage}
                 >
                     Get a new meme image
                 </button>
 
-                <img 
-                    className="meme--image"
-                    src={meme.randomImage}
-                    alt="Meme Image"
-                />
-            </div>            
+                <div className="meme">
+                    <img
+                        className="meme--image"
+                        src={meme.randomImage}
+                        alt="Meme Image"
+                    />
+
+                    <h2 className="meme--text top">
+                        {formData.topText}
+                    </h2>
+
+                    <h2 className="meme--text bottom">
+                        {formData.bottomText}
+                    </h2>
+                </div>
+            </form>
         </main>
     )
 }
+
+// const arr = [1, 5, 10, 15];
+//   console.log(arr.fill(0, 0, 4));
+
+// https://official-joke-api.appspot.com/random_joke
+// Jokes API - hundreds of jokes, returns a json file
